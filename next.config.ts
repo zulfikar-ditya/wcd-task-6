581b66f325313d16cb21c48e7ddf1dde3942a7ed
zulfikar-ditya <zulfikarditya.work@gmail.com>
2025-04-18 13:24:21 +0700

    chore: update next config for allow image url pokemons

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	images: {
+		domains: ["img.pokemondb.net"],
+	},
 };
 
 export default nextConfig;
